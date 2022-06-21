c5f05404f89cd110888a0ca22f74b2eb14ca9201
Alona <alyonagarnyk@gmai.com>
2022-06-21 09:28:53 +0300

    npm

---
+++ index.js
@@ -0,0 +1,30 @@
+// import {add, divide, multiply, subtract} from './script.js'
+
+// console.log(add(2, 3))
+
+// console.log(multiply(2, 3))
+
+// import * as math from './script.js'
+
+// console.log(math)
+
+// console.log(math.add(2, 3))
+
+// console.log(math.multiply(2, 3))
+
+// import b from './script.js'
+// console.log(b(2, 3))
+
+// import a from './script.js'
+
+// const {b, c} = a
+
+// console.log(b)
+
+// import a from './script.js'
+
+// console.log(a)
+
+import uuid from 'uuid-random';
+const a = uuid(); // 'f32dc9ae-7ca8-44ca-8f25-f258f7331c55'
+console.log(a)
---
+++ script.js
@@ -0,0 +1,35 @@
+// const add = (a, b) => a + b;
+
+// const divide = (a, b) => a / b;
+
+// const multiply = (a, b) => a * b;
+
+// const subtract = (a, b) => a - b;
+
+// export { add, divide, multiply, subtract };
+
+
+
+// const add = (a, b) => a + b;
+
+// export default add
+
+// const a = {
+//     b: 1,
+//     c: 2
+// }
+
+// const s = 'qwerty'
+
+// export default { a, s }
+
+// export default (a, b) => a + b
+
+// export default [1, 3]
+
+
+// export default {
+//     a: 3,
+//     c: 6
+// }
+
